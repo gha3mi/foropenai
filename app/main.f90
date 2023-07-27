dc4ff1347391a0e97a3595cdeb4ece29fbbf51b9
Seyed Ali Ghasemi <info@gha3mi.com>
2023-07-27 02:25:28 +0200

    Add forgpt.

---
+++ app/main.f90
@@ -0,0 +1,24 @@
+program prg_forgpt
+
+   use forgpt
+
+   implicit none
+
+   type(openai)        :: gpt
+   character(len=1000) :: user_input
+
+   call gpt%set_file_name_settings('.forgpt')
+   call gpt%set_api_key()
+   call gpt%set_org_key()
+   call gpt%list_models()
+
+   print *, "Hello! I am ChatGPT. type 'exit' to end the conversation."
+   do
+      print *, "You: "
+      read *, user_input
+      if (user_input == "exit") exit
+      call gpt%create_chat_completion(user_input,'gpt-3.5-turbo','0.7')
+   end do
+   print *, "Goodbye! Have a nice day."
+
+end program prg_forgpt
