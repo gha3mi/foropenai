c400d14d14657f7fc53630af5c8861e22999ba41
gha3mi <36126354+gha3mi@users.noreply.github.com>
2023-08-30 18:27:33 +0000

    Deploying to gh-pages from @ gha3mi/foropenai@f363cb6e3adf3de71414f0fb64881cb5ea402458 🚀

--- src/test1.f90
+++ src/test1.f90
@@ -1,14 +1,44 @@
-program test1
+program test_ChatCompletion
 
-   use foropenai, only: openai, api_key, set_api_key
+   use foropenai_ChatCompletion, only: ChatCompletion
 
    implicit none
 
-   type(openai)        :: oai
-   character(len=1000) :: user_input
+   type(ChatCompletion)           :: chat
+   character(len=1024)            :: msg
+   character(len=:), allocatable  :: rsp
 
-   api_key = set_api_key('.foropenai')
+   call chat%set_file_name('foropenai.json')
+   call chat%print_file_name()
+   call chat%load_api_key()
+   call chat%print_api_key()
+   call chat%load_organization()
+   call chat%print_organization()
 
-   call oai%model%list()
+   call chat%set_url(url='https://api.openai.com/v1/chat/completions')
 
-end program test1
+   call chat%print_model_list()
+   call chat%select_model(5)
+   call chat%print_model()
+
+   call chat%set_max_tokens(max_tokens=1)
+   call chat%print_max_tokens()
+
+   call chat%set_temperature(temperature=0.0)
+   call chat%print_temperature()
+
+   call chat%set_user_name(user_name='Ali')
+   call chat%print_user_name()
+
+   call chat%init_messages(n=2)
+   call chat%messages(1)%set_role(role='system')
+   call chat%messages(1)%set_content(content='You are a helpful assistant.')
+   call chat%messages(2)%set_role(role='user')
+   call chat%messages(2)%set_content(content='Hello?')
+   call chat%create(msg,rsp,conversiation=.false.)
+
+   call chat%usage%print_prompt_tokens()
+   call chat%usage%print_completion_tokens()
+   call chat%usage%print_total_tokens()
+
+end program test_ChatCompletion
