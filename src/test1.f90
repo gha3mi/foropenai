edea5b1c9e488f543e4457bcafbebc321c0075e3
gha3mi <36126354+gha3mi@users.noreply.github.com>
2023-09-17 11:41:07 +0000

    Deploying to gh-pages from @ gha3mi/foropenai@8a4255a1aa5db3f01576298412a3be8601e5f954 🚀

--- src/test1.f90
+++ src/test1.f90
@@ -1,46 +1,17 @@
-program test_ChatCompletion
+program test_base
 
-   use foropenai, only: ChatCompletion
+   use foropenai, only: openai
 
    implicit none
 
-   type(ChatCompletion) :: chat
+   type(openai) :: base
 
-   call chat%set_base_data('foropenai.json')
-   call chat%print_file_name()
-   call chat%print_api_key()
-   call chat%print_organization()
+   call base%set_base_data(file_name='foropenai.json')
+   
+   call base%print_api_key()
+   call base%print_organization()
+   call base%print_file_name()
 
-   call chat%set_url(url='https://api.openai.com/v1/chat/completions')
+   call base%finalize()
 
-   call chat%print_model_list()
-   call chat%select_model(5)
-   call chat%print_model()
-
-   call chat%set_max_tokens(max_tokens=1)
-   call chat%print_max_tokens()
-
-   call chat%set_temperature(temperature=0.0)
-   call chat%print_temperature()
-
-   call chat%set_user_name(user_name='Ali')
-   call chat%print_user_name()
-
-   call chat%init_messages(n=3)
-   call chat%messages(1)%set_role(role='system')
-   call chat%messages(1)%set_content(content='You are a helpful assistant.')
-   call chat%messages(2)%set_role(role='user')
-   call chat%messages(2)%set_content(content='Hello?')
-   call chat%messages(3)%set_role(role='assistant')
-   call chat%messages(3)%set_content(content='')
-   call chat%print_user_message()
-   call chat%create()
-   call chat%print_assistant_response()
-
-   call chat%usage%print_prompt_tokens()
-   call chat%usage%print_completion_tokens()
-   call chat%usage%print_total_tokens()
-
-   call chat%finalize()
-
-end program test_ChatCompletion
+end program test_base
