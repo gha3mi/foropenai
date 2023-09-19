6329791802b5690ead7640c5e1cd284a8b16e592
Seyed Ali Ghasemi <info@gha3mi.com>
2023-09-19 16:54:14 +0200

    Add Translation. (Fixes #30) Add test5. Update README.

---
+++ test/test5.f90
@@ -0,0 +1,17 @@
+program test_Translation
+
+   use foropenai, only: Translation
+
+   implicit none
+
+   type(Translation) :: trs
+
+   call trs%set_base_data(file_name='foropenai.json')
+   call trs%set(file_name='foropenai.json')
+
+   call trs%create(file='test/audio_de.mp3')
+   call trs%print_assisstant_response()
+
+   call trs%finalize()
+
+end program test_Translation
