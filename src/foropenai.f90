244076e4e610701412551a0bd7d06470a1636ad3
gha3mi <36126354+gha3mi@users.noreply.github.com>
2023-09-19 15:02:05 +0000

    Deploying to gh-pages from @ gha3mi/foropenai@98e739cd2428dd539c8db796fdc76c20223980e9 🚀

--- src/foropenai.f90
+++ src/foropenai.f90
@@ -2,5 +2,6 @@ module foropenai
     use foropenai_base
     use foropenai_ChatCompletion
     use foropenai_Transcription
+    use foropenai_Translation
     use foropenai_ImageGeneration
 end module foropenai
